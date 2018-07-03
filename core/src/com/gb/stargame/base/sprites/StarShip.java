82c6d484838e83dbff1c97c9cca482c7f0025d0c
Anna <aa.prozorova@yandex.ru>
2018-07-03 14:32:08 +0300

    пересобрала проект для Gradle 4.6; поправила движение корабля для новой системы координат.

--- core/src/com/gb/stargame/base/sprites/StarShip.java
+++ core/src/com/gb/stargame/base/sprites/StarShip.java
@@ -14,21 +14,22 @@ public class StarShip extends Sprite {
     private Vector2 buff;
     private float maxSpeed;
     private float coef;
-    private float speedX, speedY;
+    private Vector2 speed;
     private boolean movingX, movingY;
-    private float deltaX, deltaY;
-    private float cons;
-
+    private Vector2 delta;
+    private Vector2 checkSpeed;
 
     public StarShip(TextureRegion tShip, Rect worldBounds) {
         super(tShip);
         this.worldBounds = worldBounds;
-        touchPosition = new Vector2(-1, -1);
+        touchPosition = new Vector2(-1, -100);
         changeX = 1f;
-        speedX = speedY = 0f;
+        speed = new Vector2(0, 0);
+        delta = new Vector2();
         movingX = movingY = false;
         buff = new Vector2();
         coef = 0.0003f;
+        checkSpeed = new Vector2(100,100);
     }
 
     @Override
@@ -38,89 +39,96 @@ public class StarShip extends Sprite {
     }
 
     public void move(Vector2 touched) {
-        touchPosition.set(touched);
-        movingX = movingY = true;
-        updateDelta();
+        if (!isMe(touched)) {
+            touchPosition.set(touched);
+            movingX = movingY = true;
+            updateDelta();
+        }
     }
 
     public void move(Direction direction, boolean moving) {
-        deltaY = (deltaY < 0) ? -coef : coef;
-        deltaX = (deltaX < 0) ? -coef : coef;
-        touchPosition.set(-1, -1);
+        touchPosition.set(-1, -100);
+        checkSpeed = new Vector2(100,100);
+        int a = (moving) ? 1 : -1;
         switch (direction) {
             case DOWN:
-                deltaY = -Math.abs(deltaY);
+                delta.y = -coef * a;
                 movingY = moving;
                 break;
             case UP:
-                deltaY = Math.abs(deltaY);
+                delta.y = coef * a;
                 movingY = moving;
                 break;
             case LEFT:
-                deltaX = -Math.abs(deltaX);
+                delta.x = -coef * a;
                 movingX = moving;
                 break;
             case RIGHT:
-                deltaX = Math.abs(deltaX);
+                delta.x = coef * a;
                 movingX = moving;
                 break;
         }
     }
 
     public void update() {
-        float modifX = 1f, modifY = 1f;
-        if (deltaX - deltaY > coef / 2) modifY = Math.abs(deltaY / deltaX);
-        else if (deltaY - deltaX > coef / 2) modifX = Math.abs(deltaX / deltaY);
-        speedY = updateSpeed(speedY, movingY, deltaY, modifY);
-        speedX = updateSpeed(speedX, movingX, deltaX, modifX);
-        pos.x = pos.x + speedX;
+        updateSpeed();
+        pos.add(speed);
         if (getLeft() > worldBounds.getRight()) setRight(worldBounds.getLeft());
         if (getRight() < worldBounds.getLeft()) setLeft(worldBounds.getRight());
-        pos.y = pos.y + speedY;
-        if (getTop() >= worldBounds.getTop()) setTop(worldBounds.getTop());
-        if (getBottom() <= worldBounds.getBottom()) setBottom((worldBounds.getBottom()));
-        if (!touchPosition.epsilonEquals(-1, -1)) {
-            updateDelta();
+        if (getTop() >= worldBounds.getTop()) {
+            setTop(worldBounds.getTop());
+            speed.y = 0f;
+            delta.y = 0f;
+        }
+        if (getBottom() <= worldBounds.getBottom()) {
+            setBottom((worldBounds.getBottom()));
+            speed.y = 0f;
+            delta.y = 0f;
+        }
+        if (!touchPosition.epsilonEquals(-1, -100)) {
             buff.set(touchPosition);
-            if (buff.sub(pos).len() < getHeight()) { // || ((Math.abs(speedX) >= maxSpeed || Math.abs(speedY) >= maxSpeed) && (position.y == halfY || position.y == y - halfY)))
+            if (buff.sub(pos).len() < getHalfHeight()) {
                 movingY = false;
                 movingX = false;
-                touchPosition.set(-1, -1);
+                touchPosition.set(-1, -100);
+                delta.scl(-3);
+                checkSpeed.set(speed);
+                return;
             }
+            updateDelta();
         }
     }
 
-    private float updateSpeed(float v, boolean flag, float delta, float modif) {
-        if (flag) {
-            v = v + delta;
-            if (Math.abs(v) > maxSpeed) v = (v > 0) ? maxSpeed : -maxSpeed;
-            return v;
-        } else {
-            if (v >= coef) delta = coef * modif;
-            else if (v <= -coef) delta = -coef * modif;
-            else return 0.0f;
+    private void updateSpeed() {
+        if (!movingX && Math.abs(speed.x) <= coef * 2) {
+            speed.x = 0f;
+            delta.x = 0f;
+            checkSpeed.x = 100f;
         }
-        return (v - delta);
+        if (!movingY && Math.abs(speed.y) <= coef * 2) {
+            speed.y = 0f;
+            delta.y = 0f;
+            checkSpeed.y = 100f;
+        }
+        speed.add(delta);
+        if (movingX || movingY) {
+            if (Math.abs(speed.len()) > maxSpeed) {
+                float ratio = Math.abs(speed.x / speed.y);
+                int a = (speed.x >= 0) ? 1 : -1;
+                int b = (speed.y >= 0) ? 1 : -1;
+                if (Math.abs(speed.x) > Math.abs(speed.y))
+                    speed.set(a * maxSpeed, b * maxSpeed / ratio);
+                else speed.set(a * maxSpeed * ratio, b * maxSpeed);
+            }
+        } else {
+            if (Math.abs(speed.x) > Math.abs(checkSpeed.x)) delta.x *=-1;
+            if (Math.abs(speed.y) > Math.abs(checkSpeed.y)) delta.y *=-1;
+        }
     }
 
     private void updateDelta() {
         buff.set(touchPosition);
-        float tm = buff.sub(pos).len() * cons;
-        buff.set(touchPosition);
-        deltaX = 2 * (buff.sub(pos).x - speedX * tm) / (float) Math.pow(tm, 2);
-        buff.set(touchPosition);
-        deltaY = 2 * (buff.sub(pos).y - speedY * tm) / (float) Math.pow(tm, 2);
-        float ratio;
-        if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > coef * 2) {
-            ratio = deltaX / deltaY;
-            deltaX = (deltaX < 0) ? -coef * 2 : coef * 2;
-            deltaY = deltaX / ratio;
-        } else if (Math.abs(deltaY) > Math.abs(deltaX) && Math.abs(deltaY) > coef * 2) {
-            ratio = deltaY / deltaX;
-            deltaY = (deltaY < 0) ? -coef * 2 : coef * 2;
-            deltaX = deltaY / ratio;
-        }
-        System.out.println(deltaX + " " + deltaY);
+        delta.set(buff.sub(pos).scl(coef * 15));
     }
 
     public enum Direction {LEFT, RIGHT, UP, DOWN}
@@ -130,7 +138,6 @@ public class StarShip extends Sprite {
         pos.x = pos.x * worldBounds.getWidth() / changeX;
         setHeightProportion(worldBounds.getHeight() * 0.09f);
         changeX = worldBounds.getWidth();
-        cons = coef / (new Vector2(worldBounds.getWidth(), worldBounds.getHeight()).len());
-        maxSpeed = 0.01f;
+        maxSpeed = 0.009f;
     }
 }
